2c39e8a2812c1f60104a290723cec4e65fbbba65
Amjad <mdamjadmohiuddin@yahoo.com>
2018-01-01 00:46:46 +0530

    Adding test case for Thermostat file

---
+++ src/test/java/com/leanpitch/tdd/junit/ThermostatTest.java
@@ -0,0 +1,52 @@
+package com.leanpitch.tdd.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ThermostatTest {
+	
+	private Thermostat thermostat;
+
+	@Before
+	public void setUp() throws Exception {
+		thermostat = new Thermostat();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetCurrTemperature() {
+		thermostat.setDesiredTemperature(98.00F);
+		thermostat.increaseCurrTemperature(56.00F);
+		assertEquals(56.00F, thermostat.getCurrTemperature(),0);
+	}
+
+	@Test
+	public void testIncreaseCurrTemperature() {
+		thermostat.setDesiredTemperature(98.00F);
+		thermostat.increaseCurrTemperature(56.00F);
+		assertEquals(56.00F, thermostat.getCurrTemperature(),0);
+		thermostat.increaseCurrTemperature(56.00F);
+		assertEquals(98.00F, thermostat.getCurrTemperature(),0);
+	}
+
+	@Test
+	public void testGetDesiredTemperature() {
+		thermostat.setDesiredTemperature(98.00F);
+		assertEquals(98.00F, thermostat.getDesiredTemperature(),0);
+	}
+
+	@Test
+	public void testSetDesiredTemperature() {
+		thermostat.setDesiredTemperature(98.00F);
+		assertEquals(98.00F, thermostat.getDesiredTemperature(),0);
+		thermostat.setDesiredTemperature(56.00F);
+		assertEquals(56.00F, thermostat.getDesiredTemperature(),0);
+	}
+
+}
